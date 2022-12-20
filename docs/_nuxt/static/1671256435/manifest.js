031da782c6d2e11f502c15de0ece2e6fe0359da7
eiichiro <jeeeensai@gmail.com>
2022-12-20 23:15:44 +0900

    beforemount削除

--- docs/_nuxt/static/1671256435/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fgame2","\u002Finquiry","\u002Fconcept","\u002Fpicture","\u002Fschedule","\u002Fgame","\u002Fhome","\u002Finspire","\u002Fsamplepage"]})
